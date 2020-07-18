0d3edc3ead1278eb38d14fa31cee7fbf9642383d
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-07-18 13:12:02 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * Animação Scroll
+ */
+
+var $target = $(".anime"),
+animationclass = "anime-start";
+offset = $(window).height()*3/4;
+function animeScroll(){
+    var documentTop = $(document).scrollTop();
+    
+    $target.each(function(){
+        var itemTop = $(this).offset().top;
+        if(documentTop > itemTop - 350){
+            $(this).addClass(animationclass);
+        }else{
+            $(this).removeClass(animationclass);
+        }
+    })
+}
+animeScroll();
+
+$(document).scroll(function(){
+    animeScroll();
+})

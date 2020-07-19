0f9dec6dcb74b9a26c1838a00dabdb78e04655cd
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-07-19 11:55:12 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,23 +2,45 @@
  * Animação Scroll
  */
 
-var $target = $(".anime");
-var animationclass = "anime-start";
-offset = $(window).height()*3/4;
-function animeScroll(){
-    var documentTop = $(document).scrollTop();
+// var $target = $(".anime");
+// var animationclass = "anime-start";
+// offset = $(window).height()*3/4;
+// function animeScroll(){
+//     var documentTop = $(document).scrollTop();
     
-    $target.each(function(){
-        var itemTop = $(this).offset().top;
-        if(documentTop > itemTop - 350){
-            $(this).addClass(animationclass);
-        }else{
-            $(this).removeClass(animationclass);
-        }
+//     $target.each(function(){
+//         var itemTop = $(this).offset().top;
+//         if(documentTop > itemTop - 350){
+//             $(this).addClass(animationclass);
+//         }else{
+//             $(this).removeClass(animationclass);
+//         }
+//     })
+// }
+// animeScroll();
+
+// $(document).scroll(function(){
+//     animeScroll();
+// })
+
+
+/**
+ * Scroll
+ */
+
+const seta = document.querySelector('.bi-chevron-double-down')
+
+seta.addEventListener('click', scrollTo);
+
+
+
+
+function scrollTo(event){
+    event.preventDefault();
+    const el = document.querySelector('.display-5').offsetTop
+    window.scroll({
+        top: el - 120,
+        behavior: "smooth"
     })
 }
-animeScroll();
 
-$(document).scroll(function(){
-    animeScroll();
-})
